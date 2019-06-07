73fccf8624a40a5a3a323f48b96deacfbd2a6838
Monique Vitche <mvitche7@gmail.com>
2019-06-07 01:56:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
-current_player = ["X", "O"]
-turn_count = 1
-current_player.each do |player|
-  puts "X || O"
-  turn_count += 1
+def current_player = ["X", "O"]
+def turn_count = 1
+  current_player.each do |player|
+    puts "X || O"
+    turn_count += 1
+  end
 end
